d7318630795c5c02021840f5f1d2c471f0c59cbf
Kavin Fernandez Abregana <kavin.abregana@yotpo.com>
2025-04-19 02:36:55 +0800

    init files

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "4334980366413";
+  let ProductId = "6772370276399";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
